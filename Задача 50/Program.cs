af83cb1f94cde7731b34a31a2152d0e5c65817f1
Mark Derkachev <markderkachevazaza@mail.ru>
2022-09-12 14:48:24 +0300

    Решение задачи 50

---
+++ Задача 50/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента
+//  в двумерном массиве, и возвращает 
+// значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1, 7 -> такого элемента в массиве нет
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+int[,] numbers = new int[3, 4];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+Console.WriteLine("Введите номер строки: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер столбца: ");
+int col = Convert.ToInt32(Console.ReadLine());
+if (row > numbers.GetLength(0) || col > numbers.GetLength(1))
+{
+    Console.WriteLine("Такого элемента нет");
+}
+else
+{
+    Console.WriteLine($"Значение элемента {row} строки и {col} столбца равно {numbers[row - 1, col - 1]} ");
+}
