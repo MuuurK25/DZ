db2e12259a004c6c01ca742a59fad2ed690bbc24
Mark Derkachev <markderkachevazaza@mail.ru>
2022-09-13 02:06:06 +0300

    Решение задачи 62

---
+++ Задача 62/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+System.Console.WriteLine("Введите размерность массива: ");
+int len = Convert.ToInt32(Console.ReadLine());
+void FillMatrixSpiral(int[,] matrix, int n)
+{
+    int i = 0, j = 0;
+    int value = 1;
+    for (int e = 0; e < n * n; e++)
+    {
+        int k = 0;
+        do { matrix[i, j++] = value++; } while (++k < n - 1);
+        for (k = 0; k < n - 1; k++) matrix[i++, j] = value++;
+        for (k = 0; k < n - 1; k++) matrix[i, j--] = value++;
+        for (k = 0; k < n - 1; k++) matrix[i--, j] = value++;
+        ++i; ++j;
+        n = n < 2 ? 0 : n - 2;
+    }
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] < 10)
+            {
+                Console.Write($" {matrix[i, j]}", 4);
+                Console.Write(" ");
+            }
+            else Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+if(len == 4)
+{
+int[,] table = new int[len, len];
+FillMatrixSpiral(table, len);
+PrintMatrix(table);
+}
+else
+Console.WriteLine("Размерность массива не 4 на 4");
